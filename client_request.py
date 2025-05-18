f2718c055e357f4553752d5a9821c13bc69642ec
Juan del Carmen Grados Vasquez <juan.grados@tii.ae>
2025-05-18 16:03:27 +0400

    first commit

---
+++ client_request.py
@@ -0,0 +1,12 @@
+import requests, zipfile, io
+
+
+with open("obf_hello_linux", "rb") as f:
+    data = f.read()
+
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, mode="w") as zf:
+        zf.writestr("obf_hello_linux_zip", data)
+
+    zip_buffer.seek(0)
+    requests.post("http://localhost:8000/process", data=zip_buffer.read(), headers={"Content-Type": "application/zip"})
